bdee88698f5a3c4a297081bbd4a04a26e058359e
lior lichten <llichten29@gmail.com>
2026-07-21 15:38:04 +0200

    feat: scoring (Region, ScoreResult, ScoringService, 10 scenarios) + refactor PlacementSteps→WorldContext

--- src/test/java/com/cts/stepdefinitions/WorldContext.java
+++ src/test/java/com/cts/stepdefinitions/WorldContext.java
@@ -1,5 +1,7 @@
 package com.cts.stepdefinitions;
 
+import com.cts.domain.model.Kingdom;
+import com.cts.domain.model.ScoreResult;
 import com.cts.domain.model.Tile;
 import com.cts.domain.service.GameService;
 
@@ -8,4 +10,7 @@ public class WorldContext {
     public GameService otherGame;
     public String errorMessage;
     public String[] customPlayerNames;
+    public Kingdom kingdom;
+    public ScoreResult lastScore;
+    public ScoreResult otherScore;
 }
